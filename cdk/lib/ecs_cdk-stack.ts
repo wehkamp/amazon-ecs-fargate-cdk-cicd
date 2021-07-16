99422a639ec1a6ca948b582c6a489c98b4dd2c1d
sebi gavril <sgavril@wehkamp.nl>
2021-07-16 11:50:53 +0300

    update aws secrets

--- cdk/lib/ecs_cdk-stack.ts
+++ cdk/lib/ecs_cdk-stack.ts
@@ -169,7 +169,7 @@ export class EcsCdkStack extends cdk.Stack {
       owner: 'sebigavril',
       repo: 'amazon-ecs-fargate-cdk-cicd',
       branch: 'main',
-      oauthToken: cdk.SecretValue.secretsManager("/my/github/token"),
+      oauthToken: cdk.SecretValue.secretsManager("/sebi/github/token"),
       //oauthToken: cdk.SecretValue.plainText('<plain-text>'),
       output: sourceOutput
     });
